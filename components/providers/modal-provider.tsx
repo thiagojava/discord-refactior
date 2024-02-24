f86440c4af31e107bd2c7c65526eff1bde1d4d4c
thiagojava <thiago.dutra@grupoboticario.com.br>
2024-02-24 04:01:21 -0300

    feature: Edit Server

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,8 +2,8 @@
 
 import { useEffect, useState } from "react";
 import { CreateServerModal } from "../modals/create-server-modal";
-import { useModal } from "@/hooks/use-modal-store";
 import { InviteModal } from "@/components/modals/invite-modal";
+import { EditServerModal } from "@/components/modals/edit-server-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -19,6 +19,7 @@ export const ModalProvider = () => {
     <>
       <CreateServerModal />
       <InviteModal />
+      <EditServerModal />
     </>
   );
 };
