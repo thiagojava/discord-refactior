494f2d44b15bc3f9c91ace74a98b48ada4977961
thiagojava <thiago.dutra@grupoboticario.com.br>
2024-02-24 03:47:35 -0300

    feature:invite function

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,18 +3,22 @@
 import { useEffect, useState } from "react";
 import { CreateServerModal } from "../modals/create-server-modal";
 import { useModal } from "@/hooks/use-modal-store";
+import { InviteModal } from "@/components/modals/invite-modal";
 
 export const ModalProvider = () => {
-  const [isMounted,setIsMounted] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
     setIsMounted(true);
-  },[])
+  }, []);
 
-  if(!isMounted) {
+  if (!isMounted) {
     return null;
   }
   return (
+    <>
       <CreateServerModal />
+      <InviteModal />
+    </>
   );
 };
