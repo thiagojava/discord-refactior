69ed8dbcebea71f1e8688195214be6fdae7bd5fb
thiagojava <thiago.dutra@grupoboticario.com.br>
2024-02-26 09:36:48 -0300

    fix: ajusts build

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,16 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-    webback:(config => {
-        config.externals.push({
-            "utf-8-validate" : "commonjs utf-8-validate",
-            bufferUtil: "commonjs bufferutil"
-        })
-        return config
-    }),
+    webpack: (config) => {
+      config.externals.push({
+        "utf-8-validate": "commonjs utf-8-validate",
+        bufferutil: "commonjs bufferutil",
+      });
+  
+      return config;
+    },
     images: {
-        domains: [
-            "uploadthing.com",
-            "utfs.io"
-        ]
-    }
-};
-
-export default nextConfig;
+      domains: ["uploadthing.com", "utfs.io"],
+    },
+  };
+  
+  export default nextConfig;
+  
