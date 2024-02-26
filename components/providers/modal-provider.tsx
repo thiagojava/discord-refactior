48998f0685fc87c172fd0ed484d9ebad95743fe2
thiagojava <thiago.dutra@grupoboticario.com.br>
2024-02-26 09:04:24 -0300

    finally project

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -9,7 +9,9 @@ import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
-import { EditChannelModal } from "../modals/edit-channel-modal";
+import { EditChannelModal } from "@/components/modals/edit-channel-modal";
+import { MessageFileModal } from "@/components/modals/message-file-modal";
+import { DeleteMessageModal } from "@/components/modals/delete-message-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -32,6 +34,8 @@ export const ModalProvider = () => {
       <DeleteServerModal />
       <DeleteChannelModal />
       <EditChannelModal />
+      <MessageFileModal />
+      <DeleteMessageModal />
     </>
   );
 };
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webback:(config => {
+        config.externals.push({
+            "utf-8-validate" : "commonjs utf-8-validate",
+            bufferUtil: "commonjs bufferutil"
+        })
+        return config
+    }),
     images: {
         domains: [
             "uploadthing.com",
